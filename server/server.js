30f435690a1acb54b44f040454239c573f03f58e
Angellyn218 <angellyncervantes@yahoo.com>
2022-12-07 20:44:59 -0800

    Added bootstrap, axios, and dotenv to folder. Also fixed mispelling error

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const { ApolloServer } = requrie('apollo-server-express');
-const path = requrie('path');
+const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
 // const { authMiddleware } = require('./utils/auth');
 
 const { typeDefs, resolvers } = require('./schemas');
