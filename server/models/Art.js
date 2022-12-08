0f00f0302e795b195abb2cd61fed0a17b0771b05
Angellyn218 <angellyncervantes@yahoo.com>
2022-12-07 20:37:00 -0800

    Added Art model

--- server/models/Art.js
+++ server/models/Art.js
@@ -2,7 +2,29 @@ const { Schema, model } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
 const artSchema = new Schema({
-    // todo: fill in
+    title: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280,
+        trim: true
+    },
+    image: {
+        type: String,
+        required: true,
+        match: [/.+\.(png|svg|jpg|jpeg|gif)/, 'Must be image']
+    },
+    description: {
+        type: String
+    },
+    onsale: {
+        type: Boolean,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    }
 })
 
 const Art = model('Art', artSchema);
