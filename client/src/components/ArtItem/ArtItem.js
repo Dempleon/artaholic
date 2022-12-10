d12d703b02d904fa08c29cb9e6a63a3e5f83d46d
doldol123 <toruig59@gmail.com>
2022-12-10 12:33:44 -0800

    create ArtItem without HTML

--- client/src/components/ArtItem/ArtItem.js
+++ client/src/components/ArtItem/ArtItem.js
@@ -1,1 +1,43 @@
 //Rui
+import React from 'react';
+
+
+function ArtItem(item) {
+    const [state, dispatch] = useStoreContext()
+    // TODO: need to create a global state for useStoreContext
+
+    const {
+        image,
+        name,
+        _id,
+        price,
+        quantity,
+    } = item;
+
+    const { cart } = state
+
+    const addToCart = () => {
+        const itemInCart = cart.find((cartItem) => cartItem._id === id)
+        if (itemInCart) {
+            dispatch({
+                type: UPDATE_CART_QUANTITY,
+                _id: _id,
+                purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+            });
+            idbPromise('cart', 'put', {
+                ...itemInCart,
+                purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+            });
+        } else {
+            dispatch({
+                type: ADD_TO_CART,
+                art: { ...item, purchaseQuantity: 1 }
+            });
+            idbPromise('cart', 'put', { ...item, purchaseQuantity: 1 });
+        }
+    }
+
+    return (
+        //TODO: HTML part to show arts
+    )
+}
