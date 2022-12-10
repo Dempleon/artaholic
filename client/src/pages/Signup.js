0c135391cef4d1ed2406546cb93c395b37f214aa
Ying Ying <yingying@KuangYesMacBook.attlocal.net>
2022-12-09 20:45:05 -0800

    add import and signup function

---
+++ client/src/pages/Signup.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import Auth from '../utils/auth';
+import { ADD_USER } from '../utils/mutations';
+
+function Signup(props) {
+    const [formState, setFormState] = useState({ email: '', password: '' })
+    const [addUser] = useMutation(ADD_USER);
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        const mutationResponse = await addUser({
+            variables: {
+                email: formState.email,
+                password: formState.password,
+                firstName: formState.firstName,
+                lastName: formState.lastName,
+            },
+        });
+        const token = mutationResponse.data.addUser.token;
+        Auth.login(token);
+    };
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
+}
