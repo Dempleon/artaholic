ee520e60e3a57f517302990083b711c9b7429924
Ying Ying <yingying@KuangYesMacBook.attlocal.net>
2022-12-09 20:25:11 -0800

    add login in form and create login css file

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -3,10 +3,16 @@ import { useMutation } from '@apollo/client';
 import { Link } from 'react-router-dom';
 import { LOGIN } from '../utils/mutations';
 import Auth from '../utils/auth';
+import { validateEmail } from '../../utils/helpers';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import '../styles/login.css';
+
+
 
 function Login(props) {
     const [formState, setFormState] = useState({ email: '', password: '' });
-    const [login, {error}] = useMutation(LOGIN);
+    const [login, { error }] = useMutation(LOGIN);
 
     const handleFormSubmit = async (event) => {
         event.preventDefault();
@@ -29,5 +35,27 @@ function Login(props) {
         });
     };
 
-    
-}
+    return (
+        <section>
+            <Form onSubmit={handleFormSubmit}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control onChange={handleChange} type="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control onChange={handleChange} type="password" placeholder="Password" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+        </section>
+    )
+};
+
+export default Login;
