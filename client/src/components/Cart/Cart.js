92c7548d02652dea98d1a3fd54a91126a456196d
Dempoleon <dempoleon@gmail.com>
2022-12-10 18:33:42 -0800

    added queries.js to utils and a bit of starter code

--- client/src/components/Cart/Cart.js
+++ client/src/components/Cart/Cart.js
@@ -7,9 +7,14 @@ import { useStoreContext } from '../../utils/GlobalState';
 import './style.css';
 
 // todo: change the imported queries/mutations later
-// import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
+// todo: refactor as needed
+import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
 // import { QUERY_CHECKOUT } from '../../utils/queries';
 // import { idbPromise } from '../../utils/helpers';
 
 // todo: may need to change api key into loadstrip
 const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
+
+export default function Cart() {
+
+}
