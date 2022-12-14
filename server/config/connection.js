9b7094fe29d8ef3200881db1f033d8a093e1f071
Dempoleon <dempoleon@gmail.com>
2022-12-14 13:54:35 -0800

    deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-// deploying 12/13/22 10:44pm
+// deploying 12/14/22 1:54pm
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb+srv://demp:artaholics123@cluster1.qxttopw.mongodb.net/?retryWrites=true&w=majority',
   {
