44f9836791b5f853ce16a7e32bc2c5405528853c
Dempoleon <dempoleon@gmail.com>
2022-12-13 21:59:40 -0800

    deplying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-// deploying 12/13/22 9:56pm
+// deploying 12/13/22 9:59pm
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/artaholics_db',
   {
