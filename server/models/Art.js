6a540c633c74923cf5be5a202558083ddc1132f2
doldol123 <toruig59@gmail.com>
2022-12-14 00:26:11 -0800

    change onsale to quantity to fix quantity display issue

--- server/models/Art.js
+++ server/models/Art.js
@@ -1,34 +1,37 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 const artSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-        minlength: 1,
-        maxlength: 280,
-        trim: true
-    },
-    image: {
-        type: String,
-        required: true,
-        match: [/.+\.(png|svg|jpg|jpeg|gif)/, 'Must be image']
-    },
-    description: {
-        type: String
-    },
-    onsale: {
-        type: Boolean,
-        required: true
-    },
-    price: {
-        type: Number,
-        required: true
-    }, 
-    category: {
-        type: String
-    }
-})
+  name: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280,
+    trim: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    match: [/.+\.(png|svg|jpg|jpeg|gif)/, "Must be image"],
+  },
+  description: {
+    type: String,
+  },
+  quantity: {
+    type: Number,
+    min: 0,
+    default: 0,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: Schema.Types.ObjectId,
+    ref: "Category",
+    required: true,
+  },
+});
 
-const Art = model('Art', artSchema);
+const Art = model("Art", artSchema);
 
 module.exports = Art;
