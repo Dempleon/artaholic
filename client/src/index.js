b73aec1410fb2b29a323de6b6c51db2abcfc74c2
Dempoleon <dempoleon@gmail.com>
2022-12-14 19:09:08 -0800

    redoing 1 step and try to deploy to heroku

--- client/src/index.js
+++ client/src/index.js
@@ -4,20 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter } from 'react-router-dom';
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
 ReactDOM.render(
-  <BrowserRouter>
+  <React.StrictMode>
     <App />
-  </BrowserRouter>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
