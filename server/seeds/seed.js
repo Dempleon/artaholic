8e47252fb577aef99f92444485505ded47e5794a
Ying Ying <yingying@KuangYesMacBook.attlocal.net>
2022-12-14 17:57:31 -0800

    add ying artwork to local files

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -19,124 +19,124 @@ db.once("open", async () => {
 
   const arts = await Art.insertMany([
     {
-      name: "Art 1",
+      name: "Brillian Start",
       description:
-        "Art 1 description",
+        "Oil on canvas",
       image:
-        "https://indianartideas.in/images/blog/Bonjour,%20Monsieur%20Courbet,%201854.%20A%20Realist%20painting%20by%20Gustave%20Courbet.png",
+        "/images/brillian-start.jpeg",
       category: categories[0]._id,
-      price: 2.99,
-      quantity: 1,
-    },
-    {
-      name: "Art 2",
-      description:
-        "Art 2 description",
-      image:
-        "https://indianartideas.in/images/blog/Sunrise,%20impressionist%20painting%20%20by%20Claude%20Monet,%201872,%20oil%20on%20canvas.png",
-      category: categories[0]._id,
-      price: 1.99,
-      quantity: 2,
-    },
-    {
-      name: "Art 3",
-      category: categories[1]._id,
-      description:
-        "Art 3 description.",
-      image:
-        "https://indianartideas.in/images/blog/Vase%20of%20Sunflowers,%201898%E2%80%9399,%20a%20painterly%20creation%20by%20Henri%20Matisse.png",
-      price: 7.99,
-      quantity: 3,
-    },
-    {
-      name: "Art 4",
-      category: categories[1]._id,
-      description:
-        "Art 4 description.",
-      image:
-        "https://indianartideas.in/images/blog/The%20Scream,%201893,%20oil,%20tempera%20and%20pastel%20on%20cardboard%20by%20Edvard%20Munch.png",
-      price: 3.99,
-      quantity: 4,
-    },
-    {
-      name: "Art 5",
-      category: categories[1]._id,
-      description:
-        "Art 5 description.",
-      image:
-        "https://indianartideas.in/images/blog/The%20Persistence%20of%20Memory%20by%20Salvador%20Dali.png",
-      price: 14.99,
+      price: 599.99,
       quantity: 5,
     },
     {
-      name: "Art 6",
-      category: categories[2]._id,
+      name: "Boundless",
       description:
-        "Art 6 description.",
+        "Oil on canvas",
       image:
-        "https://indianartideas.in/images/blog/White,%20Red%20on%20Yellow,%20Abstract%20painting%20by%20Mark%20Rothko.png",
-      price: 399.99,
-      quantity: 6,
+        "/images/boundless.jpeg",
+      category: categories[0]._id,
+      price: 299.99,
+      quantity: 5,
     },
     {
-      name: "Art 7",
-      category: categories[2]._id,
+      name: "King's Park",
+      category: categories[1]._id,
       description:
-        "Art 7 description.",
+        "Oil on canvas",
       image:
-        "https://indianartideas.in/images/blog/Photorealist%20portrait%20by%20Chuck%20Close.png",
+      "/images/kings-park.jpeg",
       price: 199.99,
-      quantity: 30,
+      quantity: 5,
     },
     {
-      name: "Art 8",
+      name: "London Court",
+      category: categories[1]._id,
+      description:
+        "Oil on canvas",
+      image:
+        "/images/london-court.jpeg",
+      price: 399.99,
+      quantity: 5,
+    },
+    {
+      name: "Looking forward...",
+      category: categories[1]._id,
+      description:
+        "Oil on canvas",
+      image:
+        "/images/look-forward.jpeg",
+      price: 299.99,
+      quantity: 5,
+    },
+    {
+      name: "Majestic",
+      category: categories[2]._id,
+      description:
+        "Oil on canvas",
+      image:
+        "/images/majestic.jpeg",
+      price: 399.99,
+      quantity: 5,
+    },
+    {
+      name: "Nutrient",
+      category: categories[2]._id,
+      description:
+        "Oil on canvas",
+      image:
+        "/images/nutrient.jpeg",
+      price: 199.99,
+      quantity: 5,
+    },
+    {
+      name: "On the way",
       category: categories[3]._id,
       description:
-        "Art 8 description.",
+        "Oil on canvas",
       image:
-        "https://indianartideas.in/images/blog/Photorealist%20portrait%20by%20Chuck%20Close.png",
-      price: 9.99,
+        "/images/on-the-way.jpeg",
+      price: 199.99,
       quantity: 7,
     },
     {
-      name: "Art 9",
+      name: "Playground",
       category: categories[4]._id,
       description:
-        "Art 9 descriptionm.",
+        "Oil on canvas",
       image:
-        "https://indianartideas.in/images/blog/Photorealist%20portrait%20by%20Chuck%20Close.png",
-      price: 1.99,
+        "/images/playground.jpeg",
+      price: 299.99,
       quantity: 8,
     },
     {
-      name: "Art 10",
+      name: "Sail",
       category: categories[4]._id,
       description:
-        "Art 10 descriptions.",
+        "Oil on canvas",
       image:
-        "https://indianartideas.in/images/blog/Photorealist%20portrait%20by%20Chuck%20Close.png",
-      price: 2.99,
-      quantity: 1000,
+        "/images/sail.jpeg",
+      price: 299.99,
+      quantity: 5,
     },
     {
-      name: "Art 11",
+      name: "The Great Wall",
       category: categories[4]._id,
       description:
-        "Art 11 description.",
+        "Oil on canvas",
       image:
-        "https://indianartideas.in/images/blog/Photorealist%20portrait%20by%20Chuck%20Close.png",
-      price: 7.99,
-      quantity: 100,
+        "/images/the-great-wall.jpeg",
+      price: 399.99,
+      quantity: 5,
     },
     {
-      name: "Art 12",
+      name: "Winter",
       category: categories[4]._id,
       description:
-        "Art 12 descriptions.",
+        "Oil on canvas",
       image:
-        "https://indianartideas.in/images/blog/Photorealist%20portrait%20by%20Chuck%20Close.png",
-      price: 9.99,
-      quantity: 600,
+        "/images/winter.jpeg",
+      price: 199.99,
+      quantity: 5,
     },
   ]);
 
