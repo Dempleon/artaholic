d1d3a7f72ba2a467bb1ad07bc405714d48fe588d
Dempoleon <dempoleon@gmail.com>
2022-12-15 09:38:51 -0800

    deploy again

--- client/src/components/ArtItem/ArtItem.js
+++ client/src/components/ArtItem/ArtItem.js
@@ -5,7 +5,7 @@ import { pluralize } from "../../utils/helpers";
 import { useStoreContext } from "../../utils/GlobalState";
 import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
-import Button from "react-bootstrap/button";
+import Button from "react-bootstrap/Button";
 
 const styles = {
   art: {
